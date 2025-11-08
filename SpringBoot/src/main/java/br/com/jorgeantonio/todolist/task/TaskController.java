9ccca33e96f548c8111cf353fc84802af4e4719e
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2025-11-08 08:59:58 -0300

    Subindo atualizações

--- SpringBoot/src/main/java/br/com/jorgeantonio/todolist/task/TaskController.java
+++ SpringBoot/src/main/java/br/com/jorgeantonio/todolist/task/TaskController.java
@@ -9,7 +9,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,5 +51,12 @@ public class TaskController {
         var idUser = request.getAttribute("idUser");
         var tasks = this.taskRepository.findByIdUser((UUID) idUser);
         return tasks;
+    }
+
+    @PutMapping("/{id}")
+    public void update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+        
+        taskModel.setId(id);
+        this.taskRepository.save(taskModel);
     }
 }
